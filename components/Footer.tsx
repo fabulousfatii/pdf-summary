9ff1d17424c5edee64ab4d68c19c9afed858a0fb
fatima saleem <fatimasaleem778899@gmail.com>
2025-09-24 13:32:55 +0500

    footer changed

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -40,7 +40,7 @@ const Footer=()=>{
                            
                           
                 <p className="leading-relaxed mt-2 text-[15px]">
-                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                    Lorem Ipsum has been the industry  standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
                 </p>
             </div>
             <ul className="items-center justify-center mt-8 space-y-5 sm:flex sm:space-x-4 sm:space-y-0">
@@ -56,7 +56,7 @@ const Footer=()=>{
             </ul>
             <div className="mt-8 items-center justify-between sm:flex">
                 <div className="mt-4 sm:mt-0">
-                    &copy; 2022 Float UI All rights reserved.
+                    &copy; 2022 Float hbj UI All rights reserved.
                 </div>
                 <div className="mt-6 sm:mt-0">
                     <ul className="flex items-center space-x-4">
